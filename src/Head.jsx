bf0af2b5058588b4c2321fd087596171c62488b0
Ebru <yerdebru@gmail.com>
2023-09-21 23:14:04 +0300

    new changes

--- src/Head.jsx
+++ src/Head.jsx
@@ -1,14 +1,22 @@
 export default function Head() {
   return (
     <div className="Head-Container">
-      <img src="myImage.jpg" className="myImage" alt="Owner's Image" />
+      <img
+        src="src/assets/myImage.jpg"
+        className="myImage"
+        alt="Owner's Image"
+      />
       <h1>Ebru Yerdelen</h1>
       <h2>Frontend Developer</h2>
       <h3>ebruyerdelen.website</h3>
       <div className="Btns">
         <button className="Mail">
-          <a href="mailto:namesurname@email.com" target="_blank">
-            <img src="Mail.png" />
+          <a
+            href="mailto:namesurname@email.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="src/assets/Mail.png" />
             Email
           </a>
         </button>
@@ -16,8 +24,9 @@ export default function Head() {
           <a
             href="https://www.linkedin.com/in/ebru-yerdelen-aa7866285/"
             target="_blank"
+            rel="noreferrer"
           >
-            <img src="linkedin.png" />
+            <img src="src/assets/linkedin.png" />
             Linkedin
           </a>
         </button>
--- src/Last.jsx
+++ src/Last.jsx
@@ -6,22 +6,22 @@ export default function Last() {
           href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoidHIifQ%3D%3D%22%7D"
           target="_blank"
         >
-          <img src="TwitterIcon.png"></img>
+          <img src="src/assets/TwitterIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://tr-tr.facebook.com/" target="_blank">
-          <img src="FacebookIcon.png"></img>
+          <img src="src/assets/FacebookIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://www.instagram.com/ebru.yerd/" target="_blank">
-          <img src="InstagramIcon.png"></img>
+          <img src="src/assets/InstagramIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://github.com/EbruYerdelen" target="_blank">
-          <img src="GitHubIcon.png"></img>
+          <img src="src/assets/GitHubIcon.png"></img>
         </a>
       </div>
     </div>
