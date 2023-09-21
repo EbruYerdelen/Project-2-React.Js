63294d4f7ff02d8a5723bdc412d9c9a50eba6958
Ebru <yerdebru@gmail.com>
2023-09-21 20:05:36 +0300

    changes

--- src/Head.jsx
+++ src/Head.jsx
@@ -1,18 +1,14 @@
 export default function Head() {
   return (
     <div className="Head-Container">
-      <img
-        src="src/assets/images/myImage.jpg"
-        className="myImage"
-        alt="Owner's Image"
-      />
+      <img src="myImage.jpg" className="myImage" alt="Owner's Image" />
       <h1>Ebru Yerdelen</h1>
       <h2>Frontend Developer</h2>
       <h3>ebruyerdelen.website</h3>
       <div className="Btns">
         <button className="Mail">
           <a href="mailto:namesurname@email.com" target="_blank">
-            <img src="src/assets/images/Mail.png" />
+            <img src="Mail.png" />
             Email
           </a>
         </button>
@@ -21,7 +17,7 @@ export default function Head() {
             href="https://www.linkedin.com/in/ebru-yerdelen-aa7866285/"
             target="_blank"
           >
-            <img src="src/assets/images/linkedin.png" />
+            <img src="linkedin.png" />
             Linkedin
           </a>
         </button>
--- src/Last.jsx
+++ src/Last.jsx
@@ -6,22 +6,22 @@ export default function Last() {
           href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoidHIifQ%3D%3D%22%7D"
           target="_blank"
         >
-          <img src="src/assets/images/TwitterIcon.png"></img>
+          <img src="TwitterIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://tr-tr.facebook.com/" target="_blank">
-          <img src="src/assets/images/FacebookIcon.png"></img>
+          <img src="FacebookIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://www.instagram.com/ebru.yerd/" target="_blank">
-          <img src="src/assets/images/InstagramIcon.png"></img>
+          <img src="InstagramIcon.png"></img>
         </a>
       </div>
       <div>
         <a href="https://github.com/EbruYerdelen" target="_blank">
-          <img src="src/assets/images/GitHubIcon.png"></img>
+          <img src="GitHubIcon.png"></img>
         </a>
       </div>
     </div>
